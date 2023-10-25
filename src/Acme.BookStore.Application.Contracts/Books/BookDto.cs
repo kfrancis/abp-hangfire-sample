0d9c2acc160d966c08c30a0c3716750dcaa13828
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-10-25 12:24:43 -0400

    initial commit

---
+++ src/Acme.BookStore.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Books
+{
+    public class BookDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
